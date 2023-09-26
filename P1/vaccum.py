db359b571588b7990f0a98892e801c0ed15daca5
danparseg <daniel.parra.segovia@gmail.com>
2023-09-26 18:36:10 +0200

    brains :3

--- P1/vaccum.py
+++ P1/vaccum.py
@@ -27,7 +27,6 @@ class MapCell:
         else:
             self.tags["obstacle"] = False
             self.tags["dirty"] = True
-        self.tags["return_point"] = False
         self.tags["robot"] = False
 
     def __str__(self):
@@ -85,21 +84,89 @@ class MapGrid:
 
         return neighbors
     
+    def path_distance(self, start, end):
+        """
+        return the distance between two cells
+        sqrt((x1-x2)^2 + (y1-y2)^2)
+        """
+        return ((start[0]-end[0])**2 + (start[1]-end[1])**2)**0.5
+    
+    def lowest_f_score(self, open_set, f_score):
+        """
+        return the cell with the lowest f_score
+        f_score is the sum of g_score and the heuristic
+        g_score is the distance from the start cell to the current cell
+        """
+        lowest_f_score = None
+        for cell in open_set:
+            if(lowest_f_score == None):
+                lowest_f_score = cell
+            elif(f_score[cell] < f_score[lowest_f_score]):
+                lowest_f_score = cell
+        return lowest_f_score
+    
+    def path_to(self, start, end):
+        """
+        return the path from start to end
+        the algorithm is A*, the heuristic is the manhattan distance
+        g_score is the distance from the start cell to the current cell
+        f_score is the sum of g_score and the heuristic
+        """
+        #print(f"start: {start}, end: {end}")
+        open_set = set()
+        open_set.add(start)
+        came_from = {}
+        g_score = {}
+        g_score[start] = 0
+        f_score = {}
+        f_score[start] = self.path_distance(start, end)
+        while(len(open_set) > 0):
+            #get the cell with the lowest f_score
+            current = self.lowest_f_score(open_set, f_score)
+            if(current == end):
+                #reconstruct the path
+                path = []
+                while(current in came_from):
+                    path.append(current)
+                    current = came_from[current]
+                path.append(start)
+                path.reverse()
+                return path
+            open_set.remove(current)
+            neighbors = self.get_neighbors(current)
+            for neighbor in neighbors:
+                if(self.grid[neighbor[0]][neighbor[1]].tags["obstacle"]):
+                    continue
+                tentative_g_score = g_score[current] + 1
+                if(neighbor not in g_score or tentative_g_score < g_score[neighbor]):
+                    came_from[neighbor] = current
+                    g_score[neighbor] = tentative_g_score
+                    f_score[neighbor] = g_score[neighbor] + self.path_distance(neighbor, end)
+                    if(neighbor not in open_set):
+                        open_set.add(neighbor)
+        return None
+        
     def closest_dirty_cell(self,position):
         """
         return the closest dirty cell
-        for each dirty cell in the map, calculate the distance to the current cell, use the manhattan distance
-        sort the list of cells by distance
-        return the closest dirty cell
+        for each dirty cell in the map, calculate the distance to the current cell, using the A* algorithm
+        return the path to the closest dirty cell
+        don't sort the list, get the minimum value
         """
-        dirty_cells = []
+        min_distance = 10000000
+        shortest_path = []
         for x in range(self.width):
             for y in range(self.height):
-                if(self.grid[x][y].tags["dirty"] and self.grid[x][y].tags["return_point"]):
-                    distance = np.sqrt((x-position[0])**2 + (y-position[1])**2)
-                    dirty_cells.append((distance, (x,y)))
-        dirty_cells.sort(key=lambda x: x[0])
-        return dirty_cells[0][1]
+                if(self.grid[x][y].tags["dirty"]):
+                    if(self.path_distance(position, (x,y)) > min_distance):
+                        continue
+                    path=self.path_to(position, (x,y))
+                    if(min_distance > len(path)):
+                        min_distance = len(path)
+                        shortest_path = path
+        return shortest_path
+
+        
         
 
     
@@ -122,24 +189,23 @@ class Robot:
             self.move(dirty_cells[0])
             return
         #the robot is stuck, move to the closest dirty cell
-        dirty_cells.append(self.map.closest_dirty_cell(self.position))
-        self.move(dirty_cells[0])
+        path=self.map.closest_dirty_cell(self.position)
+        for position in path:
+            self.move(position)
         return
+    
     def move(self, position):
         #clean the current cell
         self.map.grid[self.position[0]][self.position[1]].edit_tag("dirty", False)
-        #set the neighbors of the current cell as return points if they are dirty
-        neighbors = self.map.get_neighbors(self.position)
-        for neighbor in neighbors:
-            if(self.map.grid[neighbor[0]][neighbor[1]].tags["dirty"]):
-                self.map.grid[neighbor[0]][neighbor[1]].edit_tag("return_point", True)
-
         #remove the robot from the current cell
         self.map.grid[self.position[0]][self.position[1]].edit_tag("robot", False)
         #move the robot
         self.position = position
         #add the robot to the new cell
         self.map.grid[self.position[0]][self.position[1]].edit_tag("robot", True)
+
+        time.sleep(0.02)
+        print(map)
 
     
 
@@ -150,10 +216,24 @@ if __name__ == "__main__":
         #convert image to numpy array
         image_data = np.asarray(image)
     #create map grid
-    map = MapGrid(image_data, 20)
+    map = MapGrid(image_data, 15)
     #create robot
-    robot = Robot(map, (3,2))
+    robot = Robot(map, (5,5))
     for i in range(1000):
         robot.step()
-        print(map)
-        time.sleep(0.2)
+        
+"""
+26-sep: I started with my navigation algorithm. I made 3 classes
+class MapGrid: it represents the map, it has a grid of cells
+class MapCell: it represents a cell, it has a position and tags
+class Robot: it represents the robot, it has a position and a map
+
+The map is generated from an image, where the black pixels are obstacles and the white pixels are free spaces.
+The robot can move in 4 directions (up, down, left, right).
+When the robot moves, it cleans the current cell and moves to a dirty neighbour cell.
+The robot tends to form a spiral, but it eventually gets stuck.
+If there are no dirty neighbour cells, the robot moves to the closest dirty cell.
+The closest dirty cell is calculated using the A* algorithm.
+The robot moves to the closest dirty cell using the shortest path dictated by the A* algorithm.
+"""
+        
